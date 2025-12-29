5060f12bedc1276463202a44b1dedcc3be629725
Logan Payton <Logan.Payton@javs.com>
2025-12-29 13:30:38 -0500

    wip

--- inspect.sh
+++ inspect.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-wine ./sandbox/win64/ffmpeg_git_lgpl_n4.3.1_shared/bin/ffmpeg.exe 2>&1
+wine ./sandbox/win64/ffmpeg_git_lgpl_n4.3.1_shared/bin/ffmpeg.exe 2>&1 | grep configuration | sed 's/ /\n/g' > inspect.args
+
